e988cb42daf9768d9836a4fe452c5416b011c2fd
Gvidas Rimkus <gvidasrimkus88@gmail.com>
2026-02-23 23:31:48 +0100

    docker container builds successfully in jenkins - now i add the actual run into the pipeline aswell.

--- pipeline.sh
+++ pipeline.sh
@@ -1,5 +1,8 @@
 # Build Docker Container
 docker build --network=host -t dvml_gruppe_docker -f Dockerfile .
 
+# Run Docker Container
+docker run --gpus all dvml_gruppe_docker
+
 # End of pipeline
 echo "\nEnd of pipeline"
